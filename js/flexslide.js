dcde844a85916933de9bb52d8ff13d0449618bc0
Duc laptop <antonio.tran.ngoc@gmail.com>
2018-05-02 23:48:48 +0700

    cover template done

---
+++ js/flexslide.js
@@ -0,0 +1,6 @@
+$(window).load(function () {
+    $('.flexslider').flexslider({
+        controlsContainer: $(".custom-controls-container"),
+        customDirectionNav: $(".custom-navigation a")
+    });
+});
