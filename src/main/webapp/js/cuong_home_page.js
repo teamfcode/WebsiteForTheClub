3234340e554226bd32d3823ef0caf2b916b16318
KomodoVu <37296951+KomodoVu@users.noreply.github.com>
2018-05-17 16:16:33 +0700

    fixed layout and responsive bug, added comment

--- src/main/webapp/js/cuong_home_page.js
+++ src/main/webapp/js/cuong_home_page.js
@@ -28,48 +28,12 @@ $(window).resize(function(){
 	}
 });
 
-//Footer form valadation
-var comment = $('.comment_c');
-var postThis = $('.post_this_c')
-
-jQuery.validator.addMethod("lettersonly", function(value, element) 
-{
-return this.optional(element) || /^[A-Z ]+$/i.test(value);
-});
-
-$("form[name='message']").validate({
-	onfocusout: false,
-	onkeyup: false,
-	onclick: false,
-    rules: {
-      name: {
-        required: true,
-        lettersonly: true
-      },
-      email: {
-        required: true,
-        email: true
-      },
-    },
-    messages: {
-      name: {
-        required: "Please fill the required field.",
-        lettersonly: "Letters and spaces only please."
-      },
-      email: {
-        required: "Please fill the required field.",
-        email: "Email address seems invalid."
-      },
-    },
-	errorPlacement: function(label, element) {
-        label.insertAfter(element);
-    },
-    submitHandler: function() {
-		$("input[type=text], textarea").val("");
-    }
-});
 
 //Auto-expand textarea for comment
+
+var comment = $('.comment_c');
+var postThis = $('.post_this_c');
+
 comment.on('keyup',function(){
 	$(this).css('height','8.5vmin');
 	$(this).height(this.scrollHeight);
